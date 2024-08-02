9ccc732c63f5a2c6f1b3430628242977fc82f785
Sandeep <sandeepyadav91577@gmail.com>
2024-08-02 17:49:50 +0530

    Initial commit

---
+++ notes_3/(1)JS_variables.js
@@ -0,0 +1,53 @@
+/*
+
+1. variable, Datatypes, objects, operators
+2. if, switch and loops
+3. funtions, strings
+4. Arrays
+5. DOM
+6. Events, setInterval, setTimeout
+7. Async/await and API
+8. try catch error handling
+9. OOP
+10. Advanced JS
+
+*/
+
+
+
+/* for printing, and input */
+/*
+alert("Hello!");
+document.write("hello!");
+console.log("Hello!!");
+console.error("hello!");
+var a = prompt("Enter your number: ");
+var b = confirm("You sure want to leave this page?");
+*/
+
+
+/* var, let, const and typeof */
+/*
+var a = "Hello";
+let b = 13;
+const c = 13.5;
+let x = true;
+let y = undefined;
+let z = null;
+
+console.log(a, b, c, x, y, z);
+console.log(typeof(a), typeof b, typeof c, typeof x, typeof y, typeof z);
+*/
+
+
+/* object */
+
+let o = {
+    "Sandeep": 18,
+    "Friend": 20,
+    "Naruto": 16
+}
+console.log(o);
+
+o.obito = 20;
+console.log(o);
